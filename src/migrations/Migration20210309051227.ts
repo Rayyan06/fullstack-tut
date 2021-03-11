c93542d19345b17b624c6f4b5203ad86fba346f1
repl.it user <replituser@example.com>
2021-03-11 02:01:18 +0000

    I am trying

--- src/migrations/Migration20210309051227.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20210309051227 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('create table "post" ("id" serial primary key, "title" varchar(255) not null, "created_at" jsonb not null, "updated_at" jsonb not null);');
-  }
-
-}
--- src/migrations/Migration20210311015910.ts
+++ src/migrations/Migration20210311015910.ts
@@ -1,6 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20210309051410 extends Migration {
+export class Migration20210311015910 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('create table "post" ("id" serial primary key, "title" text not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);');
