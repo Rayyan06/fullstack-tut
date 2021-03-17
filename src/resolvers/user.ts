59b4ebc530f903af0457a9ea7791e46a5e0ba6b1
Rayyan Khan <58400533+Rayyan06@users.noreply.github.com>
2021-03-17 13:58:56 -0400

    Update user.ts

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, ObjectType,InputType, Field, Arg, Ctx } from "type-graphql"
+import { Resolver, Mutation, ObjectType,InputType, Field, Arg, Ctx, Query } from "type-graphql"
 import { MyContext } from "../types"
 import { User } from "../entities/User"
 import argon2 from "argon2";
@@ -31,10 +31,24 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Query(() => User, {nullable: true})
+    async me (
+        @Ctx() { req, em } : MyContext
+    ) {
+        // you are not logged in
+        if (!req.session.userId) {
+            return null
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId })
+        
+        return user;
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() {em}:MyContext) {
+        @Ctx() {em, req}:MyContext) {
             if (options.username.length<=2) {
                return {
                    errors: [{
@@ -66,6 +80,12 @@ export class UserResolver {
                 }
                 console.error(err.message)
             }
+        
+        // store user id session
+        // this will set a cookie on the user
+        // logs them in
+        req.session.userId = user.id;
+        
         return { user };
     }
     @Mutation(()=>UserResponse)
